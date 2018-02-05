e8dbbfb43090868159cbbfc638d2e7349cb2b52a
Cédric Meuter <cedric.meuter@worldline.com>
2018-02-05 11:16:39 +0100

    removed spurious print

--- citizenshell/logger.py
+++ citizenshell/logger.py
@@ -8,13 +8,13 @@ class Logger(Thread):
         self._content = []
         self.start()
 
-    def run(self):
+    def run(self):         
         while True:
             line = self._input.readline()
+            if isinstance(line, bytes): line = line.decode('utf-8')
             if line == "": break
             line = line.rstrip()
             self._content.append(line)
-            print line
 
     def get_content(self):
         return self._content
