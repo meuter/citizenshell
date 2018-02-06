28f6497bba321ee157bd0d5bb283727578a1b0b8
Cédric Meuter <cedric.meuter@worldline.com>
2018-02-06 08:50:54 +0100

    Internal: AbstractShell directly derives from dict now to manage global env variable, which is simpler and less code

--- citizenshell/abstractshell.py
+++ citizenshell/abstractshell.py
@@ -3,10 +3,10 @@ from copy import deepcopy
 from .shellerror import ShellError
 
 
-class AbstractShell:
+class AbstractShell(dict):
 
     def __init__(self, check_xc=False, check_err=False, **kwargs):
-        self._global_env = kwargs
+        dict.__init__(self, kwargs)
         self._local_env = {}
         self._check_xc = check_xc
         self._check_err = check_err
@@ -25,30 +25,16 @@ class AbstractShell:
         return result
 
     def get_global_env(self):
-        return self._global_env
+        return self
 
     def get_local_env(self):
         return self._local_env
 
     def get_merged_env(self):
-        env = deepcopy(self._global_env)
+        env = dict(self)
         env.update(self._local_env)
         return env
 
     def execute_command(self, cmd):
         raise NotImplemented("this method must be implemented by the subclass")
 
-    def __setitem__(self, key, value):
-        self._global_env[key] = value
-
-    def __getitem__(self, key):
-        return self._global_env[key]
-
-    def __delitem__(self, key):
-        del self._global_env[key]
-    
-    def __contains__(self, key):
-        return key in self._global_env
-
-    def get(self, key, default=None):
-        return self._global_env.get(key, default)
