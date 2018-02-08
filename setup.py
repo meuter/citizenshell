4989c0f03e8858b6285c6c69459086609b4eb397
Cédric Meuter <cedric.meuter@gmail.com>
2018-02-08 22:32:34 +0100

    version 0.3.4

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 from setuptools import setup
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+
 setup(
     name='citizenshell',
-    version='0.3.3',
+    version='0.3.4',
     packages=['citizenshell'],
     url='https://github.com/meuter/citizenshell',
     license='MIT',
     author='Cedric Meuter',
     author_email='cedric.meuter@gmail.com',
     description='Interact with shell locally or over different connection types (telnet, ssh, serial, adb)',
+    long_description=long_description,
     keywords=["shell", "telnet", "adb", "ssh", "serial"],
     classifiers=[],
-    download_url="https://github.com/meuter/citizenshell/archive/0.3.3.tar.gz",
+    download_url="https://github.com/meuter/citizenshell/archive/0.3.4.tar.gz",
     install_requires=[
-        'termcolor',
-        'paramiko'
+        'termcolor>=1.1.0',
+        'paramiko>=2.4.0'
     ]
 )
