a8c4dc70b9168d3ddd35057e1711669908dc0d5d
Cédric Meuter <cedric.meuter@worldline.com>
2018-02-08 15:51:18 +0100

    added disconnect and reboot_wait_and_reconnect method to TelnetShell

--- citizenshell/telnetshell.py
+++ citizenshell/telnetshell.py
@@ -16,20 +16,30 @@ class TelnetShell(AbstractShell):
         self._port = port
         self._telnet = Telnet()
         self._prompt = str(uuid4())
-        self._connect()
+        self._is_connected = False
+        self.connect()
         self._inject_env(self.get_global_env())
 
-    def _connect(self):
-        self._telnet.open(self._hostname, self._port)
-        self._read_until("login: ")
-        self._write(self._username + "\n")
-        if self._password:
-            self._read_until("Password: ")
-            self._write(self._password + "\n")
-        sleep(.1)
-        self._write("export PS1=%s\n" % self._prompt)
-        self._read_until(self._prompt)  # first time for the PS1
-        self._read_until(self._prompt)  # second for the actual prompt
+    def connect(self):
+        if not self._is_connected:
+            self.log_oob("connecting to '%s'..." % self._hostname)
+            self._telnet.open(self._hostname, self._port)
+            self._read_until("login: ")
+            self._write(self._username + "\n")
+            if self._password:
+                self._read_until("Password: ")
+                self._write(self._password + "\n")
+            sleep(.1)
+            self._write("export PS1=%s\n" % self._prompt)
+            self._read_until(self._prompt)  # first time for the PS1
+            self._read_until(self._prompt)  # second for the actual prompt
+            self.log_oob("connected!")
+            self._is_connected = True
+
+    def disconnect(self):
+        if self._is_connected:
+            self._telnet.close()
+            self._is_connected = False
 
     def _write(self, text):        
         self._telnet.write(text.encode('utf-8'))
@@ -84,4 +94,16 @@ class TelnetShell(AbstractShell):
         self._cleanup_env(self.get_local_env())
         self._inject_env(self.get_global_env())
         return ShellResult(cmd, out, err, xc)
+
+    def reboot_wait_and_reconnect(self, reboot_delay=40):
+        self._write("reboot\n")
+        self.log_stdin("reboot")
+        self.disconnect()
+        sleep_left=reboot_delay
+        sleep_delta=5
+        while sleep_left > 0:
+            self.log_oob("reconnecting in %d sec..." % (sleep_left))
+            sleep(sleep_delta)
+            sleep_left -= sleep_delta
+        self.connect()
 
