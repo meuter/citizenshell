5d565479e180aea323957cb7631dcce1017d60f8
Cédric Meuter <cedric.meuter@worldline.com>
2018-02-16 17:08:32 +0100

    Added possibility to configure baudrate, parity and bytesize in SerialShell

--- citizenshell/serialshell.py
+++ citizenshell/serialshell.py
@@ -2,25 +2,31 @@ from .abstractcharshell import AbstractCharacterBasedShell
 from .shellresult import ShellResult
 
 from time import sleep
-from serial import serial_for_url, PARITY_NONE, EIGHTBITS
+from serial import serial_for_url, EIGHTBITS, PARITY_NONE
 from uuid import uuid4
 
 
 class SerialShell(AbstractCharacterBasedShell):
 
-    def __init__(self, port, username=None, password=None, check_xc=False, check_err=False, **kwargs):
+    def __init__(self, port, baudrate=115200, bytesize=EIGHTBITS, parity=PARITY_NONE, username=None, password=None, check_xc=False, check_err=False, **kwargs):
         AbstractCharacterBasedShell.__init__(self, check_xc, check_err, **kwargs)
-        # TODO(cme): inject all serial param
+        
+        if bytesize not in [ 5, 6, 7, 8 ]:
+            raise ValueError("invalid bytesize '%d'" % bytesize)
+
         self._port = port
+        self._baudrate = baudrate
+        self._bytesize = bytesize
+        self._parity = parity
         self._username = username
         self._password = password        
         self._connected = False
         self.connect()
 
     def connect(self):
-        if not self._connected:
+        if not self._connected:            
             self.log_oob("connecting to '%s'..." % self._port)
-            self._serial = serial_for_url(self._port, baudrate=115200, parity=PARITY_NONE, bytesize=EIGHTBITS)
+            self._serial = serial_for_url(self._port, baudrate=self._baudrate, parity=self._parity, bytesize=self._bytesize)
             if self._username:
                 self._write("\n" * 3)
                 sleep(.1)
@@ -43,7 +49,7 @@ class SerialShell(AbstractCharacterBasedShell):
 
     def disconnect(self):
         if self._connected:
-            self.log_oob("disconnecting from '%s'..." % self._hostname)
+            self.log_oob("disconnecting from '%s'..." % self._port)
             self._cleanup_env(self.get_global_env())
             self._write("exit\n")
             self._serial.close()
