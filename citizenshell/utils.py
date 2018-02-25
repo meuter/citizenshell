f41809e067b65c670e3c6bcefc0343cf8189be40
Cédric Meuter <cedric.meuter@gmail.com>
2018-02-25 21:02:17 +0100

    Fixed old style octal constants for python 3

--- citizenshell/utils.py
+++ citizenshell/utils.py
@@ -3,24 +3,24 @@ def convert_permissions(permissions):
     assert len(permissions) == 10
 
     if (permissions[1] == "r"): 
-        result = result | 0400
+        result = result | 0o400
     if (permissions[2] == "w"):
-        result = result | 0200
+        result = result | 0o200
     if (permissions[3] == "x"):
-        result = result | 0100
+        result = result | 0o100
 
     if (permissions[4] == "r"): 
-        result = result | 0040
+        result = result | 0o040
     if (permissions[5] == "w"):
-        result = result | 0020
+        result = result | 0o020
     if (permissions[6] == "x"):
-        result = result | 0010
+        result = result | 0o010
 
     if (permissions[7] == "r"): 
-        result = result | 0004
+        result = result | 0o004
     if (permissions[8] == "w"):
-        result = result | 0002
+        result = result | 0o002
     if (permissions[9] == "x"):
-        result = result | 0001
+        result = result | 0o001
 
     return result
