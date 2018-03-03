878fef381b5dcffc00beaa78391e9a7f396d2db8
Cédric Meuter <cedric.meuter@gmail.com>
2018-03-03 14:04:47 +0100

    Fixed pylint error

--- citizenshell/shellresult.py
+++ citizenshell/shellresult.py
@@ -29,7 +29,7 @@ class ShellResult():
                     if fd == 2: err_left = False
                     if fd == 0: process_finished = True
                     continue                
-                    
+
                 if fd == 0:
                     self._xc = line
                     continue
@@ -44,10 +44,11 @@ class ShellResult():
                     if self._check_err:                        
                         err_detected = ShellError(self.command(), "stderr '%s'" % line)
                         if not self._wait:
-                            raise err_detected
+                            raise err_detected  # pylint: disable-msg=E0702
 
             self._finished = True
-            if err_detected: raise err_detected
+            if err_detected: 
+                raise err_detected  # pylint: disable-msg=E0702
 
     def iter_stdout(self):
         for fd, line in self.iter_combined():
